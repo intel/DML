99551697cd9995b2edb4c89a27b985f0125efc3f
Hamish Nicholson <hmnicholson12@gmail.com>
2023-04-18 23:54:55 +0200

    [build] add partial clang support
    Primary change is checking for Clang compiler ID.
    DML_SECURITY_OPTIONS needed to be broken up into link and compile
    options as clang raises warnings for unused linker flags when
    compiling with the -c flag. Gtest uses -Werror, which creates errors
    preventing tests from being built.
    
    Several small bugs also needed to be fixed to compile the core
    library as well as the examples (mostly clang template deduction).
    I was unable to get benchmarks to compile due to some quite complex
    template issues in benchmarks/include/utility.hpp

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -1,8 +1,9 @@
 # Copyright (C) 2021 Intel Corporation
+# Copyright (C) 2023 Hamish Nicholson
 #
 # SPDX-License-Identifier: MIT
 
-list(APPEND DML_SECURITY_OPTIONS
+list(APPEND DML_SECURITY_COMPILE_OPTIONS
     /GS
     )
 
